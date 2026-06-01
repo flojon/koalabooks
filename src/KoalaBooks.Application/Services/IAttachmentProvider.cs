33187ed6649ca337377b65667b354c3bedbdbeef
Jonas Flodén <jonas@floden.nu>
2026-06-01 23:09:25 +0200

    chore: remove obsolete AttachmentService, IAttachmentProvider, JournalEntryAttachment

--- src/KoalaBooks.Application/Services/IAttachmentProvider.cs
+++
@@ -1,6 +0,0 @@
-namespace KoalaBooks.Application.Services;
-
-public interface IAttachmentProvider
-{
-    string GetDownloadUrl(int id);
-}
--- src/KoalaBooks.Web/Services/WebAttachmentProvider.cs
+++
@@ -1,8 +0,0 @@
-using KoalaBooks.Application.Services;
-
-namespace KoalaBooks.Web.Services;
-
-public class WebAttachmentProvider : IAttachmentProvider
-{
-    public string GetDownloadUrl(int id) => $"/attachments/{id}";
-}
