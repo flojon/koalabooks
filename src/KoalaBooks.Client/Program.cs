971b47d99991cd4ab8e64938e1b3d0453a92b90a
Jonas Flodén <jonas@floden.nu>
2026-07-16 13:54:00 +0200

    Wire silent OIDC renewal for the WASM client via the built-in remote authenticator

--- src/KoalaBooks.Client/Program.cs
+++ src/KoalaBooks.Client/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -6,5 +7,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthenticationStateDeserialization();
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    // Same-origin: the Client project is always served from the Web app's own address.
+    options.ProviderOptions.Authority = builder.HostEnvironment.BaseAddress;
+    options.ProviderOptions.ClientId = "koalabooks-wasm";
+    options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.DefaultScopes.Add("email");
+});
 
 await builder.Build().RunAsync();
